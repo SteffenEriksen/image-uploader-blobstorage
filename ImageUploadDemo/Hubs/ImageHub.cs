c233aa23516b666e49410c6b1d2f41fb58d9010b
Steffen Eriksen <steri@nets.eu>
2019-08-22 17:51:23 +0200

    init uncleaned

---
+++ ImageUploadDemo/Hubs/ImageHub.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ImageUploadDemo.Hubs
+{
+    public class ImageHub : Hub
+    {
+        public async Task SendMessage(string msg)
+        {
+            await Clients.All.SendAsync("UploadedImage", msg);
+        }
+    }
+}
